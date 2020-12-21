3ec7dc2092a4356ab53c07bd57a4071f3b4f5ec7
rayanserrao <rayanserrao25@gmail.com>
2020-12-22 02:17:30 +0530

    added date created and date edited for posts

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,5 @@ class Post(models.Model):
     title = models.CharField(max_length=150)
     desc = models.TextField()
     author = models.CharField(max_length=50)
+    date_added=models.DateTimeField(auto_now_add=True)
+    date_updated=models.DateTimeField(auto_now=True)
